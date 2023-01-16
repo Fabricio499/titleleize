1c1e8bf005c6f5ba93f50a4144001d9cab0cfd33
Fabricio499 <devfabricio1@gmail.com>
2023-01-16 17:04:27 -0300

    finish commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function Titleize(name) {
+
+    const userNameHere = name.split(' ')
+
+    function tranform(text) {
+        var words = text.toLowerCase().split(" ");
+        for (var a = 0; a < words.length; a++) {
+          var w = words[a];
+          words[a] = w[0].toUpperCase() + w.slice(1);
+        }
+        return words.join(" ");
+    }
+
+    return console.log(name && userNameHere[1]?.length > 3 ? tranform(userNameHere[0]) + ' ' + tranform(userNameHere[1])
+    : tranform(userNameHere[0]) + ' ' + String(userNameHere[1]).toLowerCase() + ' ' + tranform(userNameHere[2]))
+
+}
+
+module.exports = { Titleize }
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Titleize-simple
+
+#### how to install?
+```shell
+npm i Titleize-simple
+```
+
+#### what it does?
+<p>it will take a username, and transform it into an uppercase or lowercase formatted according to its correct form and return it as it should be shown in the ui.</p>
+
+#### example 1
+```js
+Titleize('Katherina neal trudy hope')
+//~> Katherina Neal
+```
+
+#### example 2
+```js
+Titleize('RIKKI TOLLY DYAN FOSTER')
+//~> Rikki Tolly
+```
+
+#### example 3
+```js
+Titleize('marinho da silva timoteo')
+//~> Marinho da Silva
+```
